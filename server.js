7bc16c16e0a6bf9147bc54bdda6ebcabbdbab803
ixg6233 <isaiah_glaspie1@homedepot.com>
2019-11-05 17:22:54 -0500

    adding new stuff

---
+++ server.js
@@ -0,0 +1,19 @@
+var express    = require("express");
+var bodyParser = require('body-parser');
+let dbroutes = require('./routes/mySqlDbRoutesdbroutes');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+var router = express.Router();
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our dockerdashboard module apis' });
+});
+router.get('/getAllRecords',dbroutes.getAllRecords);
+app.use('/api', router);
+app.listen(5000);

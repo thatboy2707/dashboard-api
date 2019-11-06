387256ff75264be4f5eac7cf4b9cfcbcdcd31b26
ixg6233 <isaiah_glaspie1@homedepot.com>
2019-11-06 15:02:13 -0500

    adding and correcting my sql infor connection for api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express    = require("express");
-var bodyParser = require('body-parser');
-let dbroutes = require('./routes/mySqlDbRoutesdbroutes');
-var app = express();
+const express = require("express");
+const bodyParser = require('body-parser');
+let mySqlDbRoutes = require('./routes/mySqlDbRoutes');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -10,10 +10,11 @@ app.use(function(req, res, next) {
     next();
 });
 var router = express.Router();
-// test route
+
+// test the local browser with the below route
 router.get('/', function(req, res) {
-    res.json({ message: 'welcome to our dockerdashboard module apis' });
+    res.json({ message: 'This sees to be working sir' });
 });
-router.get('/getAllRecords',dbroutes.getAllRecords);
-app.use('/api', router);
+router.get('/getAllRecords',mySqlDbRoutes.getAllRecords);
+app.use('/dashboard-api', router);
 app.listen(5000);
